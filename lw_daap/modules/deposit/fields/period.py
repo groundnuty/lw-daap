d43b7164593f667181914f85da95c546ac64fc9a
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-09-09 16:01:49 +0000

    Removed comment

--- lw_daap/modules/deposit/fields/period.py
+++ lw_daap/modules/deposit/fields/period.py
@@ -4,7 +4,7 @@ from wtforms import validators, widgets
 from invenio.base.i18n import _
 from lw_daap.modules.invenio_deposit.form import WebDepositForm
 from lw_daap.modules.invenio_deposit.fields import Date
-from lw_daap.modules.invenio_deposit.field_widgets import date_widget
+from lw_daap.modules.deposit.field_widgets import date_widget
 
 
 __all__ = ['PeriodField']
@@ -13,14 +13,12 @@ class PeriodFieldForm(WebDepositForm):
     start = Date(
         label=_('Start date'),
         description='Start date.', 
-        #validators=[validators.DataRequired()],
         widget=date_widget,
         widget_classes='input-sm',
         )
     end = Date(
         label=_('End date'),
         description='End date.',
-        #validators=[validators.DataRequired()],
         widget=date_widget,
         widget_classes='input-sm',
         )
