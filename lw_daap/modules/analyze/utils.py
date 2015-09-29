731d128e253d33b0ce043d9e7ca1bfbb7ac76df9
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-09-29 22:38:32 +0100

    WIP, analyze

---
+++ lw_daap/modules/analyze/utils.py
@@ -0,0 +1,23 @@
+
+import json
+from invenio.modules.knowledge.api import get_kb_mappings
+
+def get_requirements(): 
+    reqs = dict(
+        flavors={},
+        images={},
+        app_envs={},
+    )
+    for mapping in get_kb_mappings('requirements'):
+        v = json.loads(mapping['value'])
+        if v.get('domain_flavor', False):
+            if v['flavor-id']:
+                reqs['flavors'][v['flavor-id']] = v
+        elif v.get('domain_os', False):
+            if v['image-id']:
+                reqs['images'][v['image-id']] = v
+        elif v.get('domain_app_env', False):
+            if v['app-id']:
+                reqs['app_envs'][v['id']] = v
+    return reqs
+
--- lw_daap/modules/analyze/views.py
+++ lw_daap/modules/analyze/views.py
@@ -1,9 +1,12 @@
 import json
 
-from flask import Blueprint, current_app, render_template
+from flask import Blueprint, current_app, render_template, request, redirect, url_for
 from flask_menu import register_menu
 
-from invenio.modules.knowledge.api import get_kb_mappings
+from forms import LaunchForm
+from infra import launch_vm, list_vms
+
+from lw_daap.modules.profile.decorators import delegation_required
 
 
 blueprint = Blueprint(
@@ -14,20 +17,29 @@ blueprint = Blueprint(
     static_folder='static',
 )
 
-def get_requirements(): 
-    m = get_kb_mappings('requirements')
-    return [json.loads(k['value']) for k in m] 
-
-def find_requirement(reqs, key, req_id):
-    r = next((x for x in reqs if x.get(key, None) == req_id), {})
-    return r.get('title', 'unknown')
-
 
 @blueprint.route('/')
 @register_menu(blueprint, 'main.analyze', 'Analyze', order=3)
+@delegation_required
 def index():
     ctx = dict(
-        vms=[]
+        vms=list_vms()
     )
     return render_template('analyze/index.html', **ctx)
 
+
+@blueprint.route('/launch', methods=['GET', 'POST'])
+@delegation_required
+def launch():
+    form = LaunchForm(request.form)
+    form.fill_fields_choices()
+    if form.validate_on_submit():
+        launch_vm(name=form.name.data,
+                  image=form.image.data,
+                  flavor=form.flavor.data,
+                  app_env=form.app_env.data)
+        return redirect(url_for('.index'))
+    ctx = dict(
+        form = form,
+    )
+    return render_template('analyze/launch.html', **ctx)
