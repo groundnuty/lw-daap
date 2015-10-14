651ad47b4b690ec5101115cad16531d538bc460b
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-10-14 22:30:15 +0200

    License info

--- lw_daap/modules/analyze/infra.py
+++ lw_daap/modules/analyze/infra.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Lifewatch DAAP.
+# Copyright (C) 2015 Ana Yaiza Rodriguez Marrero.
+#
+# Lifewatch DAAP is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lifewatch DAAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lifewatch DAAP. If not, see <http://www.gnu.org/licenses/>.
+
 from base64 import b64encode
 from datetime import datetime
 from itertools import chain
