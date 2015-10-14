ad3c63daba71e541341dfa61a931c1d32b08e9a4
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-10-14 23:06:18 +0000

    Use a different venv, add license

--- lw_daap/modules/analyze/templates/analyze/jupyter.sh
+++ lw_daap/modules/analyze/templates/analyze/jupyter.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# This file is part of Lifewatch DAAP.
+# Copyright (C) 2015 Ana Yaiza Rodriguez Marrero.
+#
+# Lifewatch DAAP is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lifewatch DAAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lifewatch DAAP. If not, see <http://www.gnu.org/licenses/>.
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y install build-essential python3-dev python3-pip python-virtualenv libzmq3-dev
@@ -9,8 +24,8 @@ cat > $JUP_INSTALLER << EOF
 unset XDG_RUNTIME_DIR
 unset XDG_SESSION_ID
 
-virtualenv --python=python3 jupyter
-. jupyter/bin/activate
+virtualenv --python=python3 .jupyter_venv
+. .jupyter_venv/bin/activate
 pip3 install jupyter
 jupyter notebook --generate-config
 CONFIG_FILE=\`jupyter --config-dir\`/jupyter_notebook_config.py
