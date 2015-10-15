f0f2ef11794959aa89a43133b3b754719fb1ed46
Airam Rodriguez <airamrodriguezmarrero@gmail.com>
2015-10-15 14:50:05 +0000

    License Info

--- lw_daap/testsuite/api/deposit.py
+++ lw_daap/testsuite/api/deposit.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Lifewatch DAAP.
+# Copyright (C) 2015 Ana Yaiza Rodriguez Marrero.
+#
+# Lifewatch DAAP is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lifewatch DAAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lifewatch DAAP. If not, see <http://www.gnu.org/licenses/>.
+
+
 #!/usr/bin/env python
 
 import requests
@@ -36,7 +55,7 @@ def upload(recid, rfile):
     print " --> FILE %s => %s, %s" % (rfile['filename'], r.status_code, r.reason)
   except:
     raise
-    
+
 
 def publish(recid):
   try:
@@ -52,7 +71,7 @@ if __name__ == "__main__":
     records = json.load(fd)
   except:
     raise
-    
+
   for record in records['records']:
     recid = create(json.dumps(record['metadata']))
     if recid:
@@ -61,8 +80,8 @@ if __name__ == "__main__":
         upload(recid, f)
       publish(recid)
     print '\r'
-			
-			
-	
+
+
+
 
 
