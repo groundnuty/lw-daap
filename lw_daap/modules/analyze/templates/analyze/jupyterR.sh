254707b8f3aba4e31360b27f2ff3549a5046ac56
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-10-15 21:36:40 +0000

    Not cran mirror

--- lw_daap/modules/analyze/templates/analyze/jupyterR.sh
+++ lw_daap/modules/analyze/templates/analyze/jupyterR.sh
@@ -16,22 +16,15 @@
 # along with Lifewatch DAAP. If not, see <http://www.gnu.org/licenses/>.
 
 export DEBIAN_FRONTEND=noninteractive
-echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" > /etc/apt/sources.list.d/cran
+echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" > /etc/apt/sources.list.d/cran.list
 apt-get update
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get -y install build-essential python3-dev python3-pip python-virtualenv libzmq3-dev r-base
 useradd -m jup
 
-R_INSTALLER=`mktemp`
-cat > $R_INSTALLER << EOF
-dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
-install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
-                 repos = c('http://cran.es.r-project.org/', 'http://irkernel.github.io/', getOption('repos')),
-                 type = 'source')
-IRkernel::installspec()
-EOF
-
 JUP_INSTALLER=`mktemp`
 cat > $JUP_INSTALLER << EOF
+set -x
 unset XDG_RUNTIME_DIR
 unset XDG_SESSION_ID
 
@@ -45,16 +38,22 @@ ID=\$(curl http://169.254.169.254/openstack/latest/meta_data.json | \
      python2 -c "import json; import sys; print json.load(sys.stdin)['uuid']")
 echo "c.NotebookApp.base_url = '/\$ID'" >> \$CONFIG_FILE
 
-mkdir ~/.R
-cat > ~/.R/Makeconf << COF
-CXX1X = g++ 
-CXX1XSTD = -std=c++11
-SHLIB_CXX1XLD = \$(CXX1X) \$(CXX1XSTD)
-CXX1XPICFLAGS = -fpic
-SHLIB_CXX1XLDFLAGS = -shared
-COF
+mkdir -p ~/.R
+echo 'CXX1X = g++' > ~/.R/Makeconf
+echo 'CXX1XSTD = -std=c++11' >> ~/.R/Makeconf
+echo 'SHLIB_CXX1XLD = $(CXX1X) $(CXX1XSTD)' >> ~/.R/Makeconf
+echo 'CXX1XPICFLAGS = -fpic' >> ~/.R/Makeconf
+echo 'SHLIB_CXX1XLDFLAGS = -shared' >> ~/.R/Makeconf
 
-Rscript $R_INSTALLER 
+export R_LIBS_USER=~/.R
+R_INSTALLER=\`mktemp\`
+echo 'dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)' > \$R_INSTALLER
+echo 'install.packages(c("rzmq","repr","IRkernel","IRdisplay"),' >> \$R_INSTALLER
+echo '                 repos = "http://irkernel.github.io/",' >> \$R_INSTALLER
+echo '                 type = "source")' >> \$R_INSTALLER
+echo 'IRkernel::installspec()' >> \$R_INSTALLER
+
+Rscript \$R_INSTALLER 
 
 jupyter notebook --no-browser --ip=0.0.0.0 &
 EOF
