ac836f8ad4511c0451eb380dd8f40a893bb68e76
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-10-30 23:26:42 +0000

    Add PIDs for all records. Add badge for PIDs and DOIs.base/templates/format/record/Default_HTML_detailed.tpl

---
+++ lw_daap/modules/pids/config.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-                                                         
+#                                                                               
+# This file is part of Lifewatch DAAP.                                          
+# Copyright (C) 2015 Ana Yaiza Rodriguez Marrero.                               
+#                                                                               
+# Lifewatch DAAP is free software; you can redistribute it and/or modify        
+# it under the terms of the GNU General Public License as published by          
+# the Free Software Foundation, either version 3 of the License, or             
+# (at your option) any later version.                                           
+#                                                                               
+# Lifewatch DAAP is distributed in the hope that it will be useful,             
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  
+# GNU General Public License for more details.                                  
+#                                                                               
+# You should have received a copy of the GNU General Public License             
+# along with Lifewatch DAAP. If not, see <http://www.gnu.org/licenses/>.    
+
+SHIELDSIO_BASE_URL = "https://img.shields.io/badge/"                      
+"""Base URL for shields.io."""                                                  
+                                                                                
+BADGE_STYLES = [                                                         
+    "flat",                                                                     
+    "flat-square",                                                              
+    "plastic",                                                                  
+]                                                                               
+"""List of shields.io styles."""                                                
+                                                                                
+BADGE_COLORS = [                                                         
+    "brightgreen",                                                              
+    "green",                                                                    
+    "yellowgreen",                                                              
+    "yellow",                                                                   
+    "orange",                                                                   
+    "red",                                                                      
+    "lightgrey",                                                                
+    "blue",                                                                     
+]                                                                               
+"""List of shields.io styles."""                                                
+                                                                                
+BADGE_DEFAULT_STYLE = "flat"                                             
+"""Default shields.io badge style."""                                           
+                                                                                
+BADGE_DEFAULT_COLOR = "blue"                                             
+"""Default shields.io badge color."""                
