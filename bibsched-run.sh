b9a01f1722453aa241307cc6d67a0ba6252ba138
Airam Rodriguez <airamrodriguezmarrero@gmail.com>
2015-11-04 13:51:33 +0000

    bibindex -w global

--- bibsched-run.sh
+++ bibsched-run.sh
@@ -3,6 +3,7 @@ bibsched stop
 bibsched start
 bibsched purge 
 bibindex -s5m -u admin --continue-on-error
+bibindex -s5m -u admin -w global --continue-on-error
 bibrank -s5m -u admin --continue-on-error
 bibsort -s5m -u admin --continue-on-error
 webcoll -s5m -u admin --continue-on-error
