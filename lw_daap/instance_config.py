2297015de8c3bac16f6c9cab6cff48445ff1f0dc
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-11-21 23:35:04 +0000

    New file for instance configuration

---
+++ lw_daap/instance_config.py
@@ -0,0 +1,14 @@
+
+# Add here any relevant configuration that needs to be overriden
+# for deployment
+#
+
+# Site URLs
+#CFG_SITE_URL = 'http://localhost'
+#CFG_SITE_SECURE_URL = 'https://localhost'
+
+# OpenStack Configuration
+#CFG_OPENSTACK_AUTH_URL = "https://localhost:5000/v2.0"
+#CFG_OPENSTACK_TENANT = "mytenant"
+#CFG_ANALYZE_ETCD_URL = 'http://localhost:4001'
+
