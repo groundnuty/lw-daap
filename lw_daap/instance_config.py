0f0265a39dd7fe64ab26cb473fcc6a8dd06d8e02
Ana Yaiza Rodriguez Marrero <yaiza.rodriguez@aeonium.eu>
2015-11-27 22:49:17 +0100

    Adapt for using external databases

--- lw_daap/instance_config.py
+++ lw_daap/instance_config.py
@@ -10,6 +10,28 @@ CFG_SITE_URL = 'http://localhost'
 CFG_SITE_SECURE_URL = 'https://localhost'
 
 #
+# Database configuration
+# use "db" as CFG_DATABASE_HOST if using the mysql from the docker container
+#
+CFG_DATABASE_HOST = 'db'
+CFG_DATABASE_NAME = 'lwosf'
+CFG_DATABASE_USER = 'lwosf'
+CFG_DATABASE_PASS = u'my123p$ss'
+CFG_DATABASE_TYPE = u'mysql'
+CFG_DATABASE_PORT = 3306
+
+
+#
+# emails configuration
+#
+# 'flask_email.backends.console.Mail' does not send emails but shows them in
+# the console
+# 'flask_email.backends.smtp.Mail' sends them with SMTP.
+CFG_EMAIL_BACKEND = 'flask_email.backends.console.Mail'
+
+
+
+#
 # OpenStack Configuration
 #
 #CFG_OPENSTACK_AUTH_URL = "https://localhost:5000/v2.0"
