92fb63b49de6ea9d8cb903d87b3852e0f790035c
Rafael Salas Robledo <rsalas@viavansi.com>
2015-12-16 14:11:07 +0100

    Pruebas con JS

--- lw_daap/modules/instruments/forms.py
+++ lw_daap/modules/instruments/forms.py
@@ -85,6 +85,48 @@ class InstrumentForm(Form):
         """Check if filed has autocomplete."""
         return hasattr(field, 'autocomplete')
 
+    def get_groups(self):
+        """Get a list of the (group metadata, list of fields)-tuples.
+
+        The last element of the list has no group metadata (i.e. None),
+        and contains the list of fields not assigned to any group.
+        """
+        fields_included = set()
+        field_groups = []
+
+        if hasattr(self, 'groups'):
+            for group in self.groups:
+                group_obj = {
+                    'name': group[0],
+                    'meta': CFG_GROUPS_META.copy(),
+                }
+
+                fields = []
+                for field_name in group[1]:
+                    if field_name in ['-', ]:
+                        fields.append(field_name)
+                    else:
+                        try:
+                            fields.append(self[field_name])
+                            fields_included.add(field_name)
+                        except KeyError:
+                            pass
+
+                if len(group) == 3:
+                    group_obj['meta'].update(group[2])
+
+                field_groups.append((group_obj, fields))
+
+        # Append missing fields not defined in groups
+        rest_fields = []
+        for field in self:
+            if field.name not in fields_included:
+                rest_fields.append(field)
+        if rest_fields:
+            field_groups.append((None, rest_fields))
+
+        return field_groups
+
     field_icons = {
         'name': 'fa fa-md fa-fw',
         'embargo_date': 'fa fa-calendar fa-fw',
